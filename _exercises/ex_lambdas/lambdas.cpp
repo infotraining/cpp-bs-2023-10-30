b97787c5beaa6a1fa21928b012fa39744a21a06d
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-02 11:30:05 +0100

    Lambdas - solution

--- _exercises/ex_lambdas/lambdas.cpp
+++ _exercises/ex_lambdas/lambdas.cpp
@@ -18,9 +18,7 @@ TEST_CASE("lambda exercise")
 
     SECTION("count even numbers")
     {
-        auto evens_count = 0;
-
-        // TODO - count_if
+        auto evens_count = std::count_if(data.begin(), data.end(), [](int n) { return n % 2 == 0; });
 
         REQUIRE(evens_count == 4);
     }
@@ -29,18 +27,30 @@ TEST_CASE("lambda exercise")
     {
         vector<int> evens;
 
-        // TODO - for_each
+        // std::for_each(data.begin(), data.end(), [&evens](int n) { if( n % 2 == 0) evens.push_back(n); });
+
+        std::copy_if(data.begin(), data.end(), std::back_inserter(evens), [](int n) { return n % 2 == 0; });
 
         REQUIRE_THAT(evens, Catch::Matchers::Equals(vector<int>{6, 8, 12, 10}));
     }
 
     SECTION("create container with squares")
     {
-        vector<int> squares;
+        SECTION("ver_1")
+        {
+            vector<int> squares(data.size());
+            std::transform(data.begin(), data.end(), squares.begin(), [](int n) { return n * n; });
 
-        // TODO - transform
+            REQUIRE_THAT(squares, Catch::Matchers::Equals(vector<int>{1, 36, 9, 25, 64, 81, 169, 144, 100, 2025}));
+        }
 
-        REQUIRE_THAT(squares, Catch::Matchers::Equals(vector<int>{1, 36, 9, 25, 64, 81, 169, 144, 100, 2025}));
+        SECTION("ver_2")
+        {
+            vector<int> squares;
+            std::transform(data.begin(), data.end(), std::back_inserter(squares), [](int n) { return n * n; });
+
+            REQUIRE_THAT(squares, Catch::Matchers::Equals(vector<int>{1, 36, 9, 25, 64, 81, 169, 144, 100, 2025}));
+        }
     }
 
     SECTION("calculate average")
@@ -48,12 +58,13 @@ TEST_CASE("lambda exercise")
         auto sum = 0.0;
         double avg{};
 
-        // TODO - accumulate
+        avg = std::accumulate(data.begin(), data.end(), 0.0) / data.size();
         REQUIRE(avg == Catch::Approx(11.2));
 
         SECTION("find first greater than avg")
         {
-            // TODO
+            auto pos = std::find_if(data.begin(), data.end(), [avg](int n) { return n > avg; });
+            CHECK(*pos == 13);
         }
     }
 }
