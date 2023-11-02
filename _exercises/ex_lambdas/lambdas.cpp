dae5d09e087798f32ceb24933d4b5e8cecab0569
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-02 11:00:17 +0100

    Lambdas

--- _exercises/ex_lambdas/lambdas.cpp
+++ _exercises/ex_lambdas/lambdas.cpp
@@ -1,6 +1,7 @@
+#include <algorithm>
+#include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_all.hpp>
-#include <algorithm>
 #include <cmath>
 #include <functional>
 #include <iostream>
@@ -13,8 +14,6 @@ using namespace std;
 
 TEST_CASE("lambda exercise")
 {
-    using namespace Catch::Matchers;
-
     vector<int> data = {1, 6, 3, 5, 8, 9, 13, 12, 10, 45};
 
     SECTION("count even numbers")
@@ -22,41 +21,39 @@ TEST_CASE("lambda exercise")
         auto evens_count = 0;
 
         // TODO - count_if
-        
+
         REQUIRE(evens_count == 4);
     }
 
     SECTION("copy evens to vector")
     {
         vector<int> evens;
-        
+
         // TODO - for_each
 
-        REQUIRE_THAT(evens, Equals(vector<int>{6, 8, 12, 10}));
+        REQUIRE_THAT(evens, Catch::Matchers::Equals(vector<int>{6, 8, 12, 10}));
     }
 
     SECTION("create container with squares")
     {
         vector<int> squares;
-        
+
         // TODO - transform
 
-        REQUIRE_THAT(squares, Equals(vector<int>{1, 36, 9, 25, 64, 81, 169, 144, 100, 2025}));
+        REQUIRE_THAT(squares, Catch::Matchers::Equals(vector<int>{1, 36, 9, 25, 64, 81, 169, 144, 100, 2025}));
     }
 
     SECTION("calculate average")
     {
         auto sum = 0.0;
-        double avg;
-            
+        double avg{};
+
         // TODO - accumulate
--
-        REQUIRE_THAT(avg,  WithinAbs(11.2, 0.001));
+        REQUIRE(avg == Catch::Approx(11.2));
 
         SECTION("find first greater than avg")
         {
             // TODO
         }
-
     }
 }
